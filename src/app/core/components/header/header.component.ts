e1e7892590b214bb20fbce94ed12161d5a63428e
Guil <Flashnat.br@gmail.com>
2024-07-07 23:41:36 -0300

    [feat] Criar paginas iniciais

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -3,14 +3,21 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTabsModule} from '@angular/material/tabs';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTabsModule],
+  imports: [MatToolbarModule, 
+            MatButtonModule, 
+            MatIconModule, 
+            MatTabsModule, 
+            RouterLink,
+            RouterLinkActive
+          ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  
 }
