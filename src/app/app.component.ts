092afd71272e0b52336732b6a7464dd67f2671ff
Guil <Flashnat.br@gmail.com>
2024-07-09 17:15:24 -0300

    [feat] Adicionar filtro de busca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/components/header/header.component';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [{provide: MAT_DATE_LOCALE, useValue: 'pt-BR'}]
 })
 export class AppComponent {
   
--- src/app/core/components/page-search/appointment-filter-container/appointment-filter-container.component.ts
+++ src/app/core/components/page-search/appointment-filter-container/appointment-filter-container.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { AppointmentDateRangeComponent } from "../appointment-date-range/appointment-date-range.component";
 
 
 @Component({
   selector: 'app-appointment-filter-container',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, AppointmentDateRangeComponent],
   templateUrl: './appointment-filter-container.component.html',
   styleUrl: './appointment-filter-container.component.scss'
 })
