894736d2e0ec18fbb8ec72ac18468a37244cb409
Guil <Flashnat.br@gmail.com>
2024-07-13 18:16:46 -0300

    [feat] Criar e integrar modais

---
+++ src/app/core/components/shared/modal-notification/modal-notification.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { ModalNotificationService } from '../../../services/modal-notification.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@Component({
+  selector: 'app-modal-notification',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, MatIconModule],
+  templateUrl: './modal-notification.component.html',
+  styleUrl: './modal-notification.component.scss'
+})
+export class ModalNotificationComponent {
+  private dialogRef = inject(MatDialogRef<ModalNotificationComponent>);
+  private _modalService = inject(ModalNotificationService);
+  
+  data = inject(MAT_DIALOG_DATA);
+
+  close():void{
+    this.dialogRef.close()
+  }
+}
---
+++ src/app/core/services/modal-notification.service.ts
@@ -0,0 +1,23 @@
+import { inject, Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalNotificationComponent } from '../components/shared/modal-notification/modal-notification.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalNotificationService {
+  private dialog = inject(MatDialog);
+
+  showNotification(title: string, message:string, isError:boolean):void{
+    this.dialog.open(ModalNotificationComponent,{
+      data:
+      {
+        title: title,
+        message: message,
+        isError: isError
+      }
+    })
+  }
+
+  constructor() { }
+}
