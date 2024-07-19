2460a721cc220905e3eec099be9d4191de6ecc58
Guil <Flashnat.br@gmail.com>
2024-07-19 19:30:18 -0300

    [feat] Implementar pagina Home do sistema

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
+import { LoginService } from "../../core/services/auth/login.service";
+import { TokenService } from "../../core/services/auth/token.service";
 
 @Component({
 	selector: "app-home",
@@ -7,4 +9,20 @@ import { Component } from "@angular/core";
 	templateUrl: "./home.component.html",
 	styleUrl: "./home.component.scss",
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+	private _loginService = inject(LoginService);
+	private _tokenService = inject(TokenService);
+	isLogged!: boolean;
+	name!: string;
+
+	logout() {
+		this._loginService.logout();
+	}
+
+	ngOnInit(): void {
+		this._loginService.isLogged$.subscribe(logged => {
+			this.isLogged = logged;
+		});
+		this.name = this._tokenService.getTokenName()!;
+	}
+}
